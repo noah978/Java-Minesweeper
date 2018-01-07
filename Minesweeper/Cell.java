637596f62a13af1bafcd0578c5dd3ae9d7354e15
noah978 <noahkeck@mindspring.com>
2018-01-06 23:10:27 -0600

    Added the first upload of files

---
+++ Minesweeper/Cell.java
@@ -0,0 +1,103 @@
+import greenfoot.*;  // (World, Actor, GreenfootImage, Greenfoot and MouseInfo)
+import java.util.List;
+
+/**
+ * Write a description of class Cells here.
+ * 
+ * @Noah Keck
+ * @v1.0
+ * @1/6/2018
+ */
+public class Cell extends Actor
+{
+    public String cellType;
+    public int publicNumber;
+    private MinesweeperBoard board;
+    public Cell()
+    {
+        board = (MinesweeperBoard)getWorld();
+        setImage(board.GraySquare);
+        cellType = "normal";
+    }
+    public void act() 
+    {
+        board = (MinesweeperBoard)getWorld();
+        if (board.playGame)
+            clickCheck();
+    }
+    public void setCell(String type)
+    {
+        cellType = type;
+    }
+    private void clickCheck()
+    {
+        MouseInfo ms = Greenfoot.getMouseInfo();
+        if (ms!=null){
+            if (ms.getX()==this.getX() && ms.getY()==this.getY()){
+                if (cellType.equals("normal") && ms.getButton()==1 && Greenfoot.mouseClicked(this)){
+                    setImage(board.ClickedSquare);
+                    findNumber();
+                }
+                else if (cellType.equals("flagged") && !cellType.equals("clicked") && ms.getButton()==3 && Greenfoot.mouseClicked(this)){
+                    setImage(board.GraySquare);
+                    cellType="normal";
+                }
+                else if (cellType.equals("normal") && ms.getButton()==3 && Greenfoot.mouseClicked(this)){
+                    setImage(board.Flagged);
+                    cellType="flagged";
+                }
+                else if (cellType.equals("mine") && ms.getButton()==1 && Greenfoot.mouseClicked(this)){
+                    setImage(board.RedMine);
+                    cellType="activatedMine";
+                    MinesweeperBoard world = (MinesweeperBoard)getWorld();
+                    world.GameOver();
+                }
+                else if (cellType.equals("mine") && ms.getButton()==3 && Greenfoot.mouseClicked(this)){
+                    setImage(board.Flagged);
+                    cellType="flaggedMine";
+                }
+                else if (cellType.equals("flaggedMine") && ms.getButton()==3 && Greenfoot.mouseClicked(this)){
+                    setImage(board.GraySquare);
+                    cellType="mine";
+                }
+            }
+        }
+    }
+    private void findNumber()
+    {
+        int number = findPublicNumber();
+        if (number>0)
+            assignNumberImage(number);
+        else
+            board.revealZeros(this.getX(), this.getY());
+        cellType="clicked";
+    }
+    public void assignNumberImage(int number)
+    {
+        if (number==1)
+            setImage(board.OneSquare);
+        else if (number==2)
+            setImage(board.TwoSquare);
+        else if (number==3)
+            setImage(board.ThreeSquare);
+        else if (number==4)
+            setImage(board.FourSquare);
+        else if (number==5)
+            setImage(board.FiveSquare);
+        else if (number==6)
+            setImage(board.SixSquare);
+        else if (number==7)
+            setImage(board.SevenSquare);
+        else if (number==8)
+            setImage(board.EightSquare);
+    }
+    public int findPublicNumber()
+    {
+        List<Cell> neighbors = getNeighbours(1, true, Cell.class);
+        publicNumber = 0;
+        for (Cell neighbor : neighbors)
+            if (neighbor.cellType.equals("mine") || neighbor.cellType.equals("flaggedMine"))
+                publicNumber++;
+        return publicNumber;
+    }
+}
---
+++ Minesweeper/MinesweeperBoard.java
@@ -0,0 +1,128 @@
+import greenfoot.*;
+import static greenfoot.Greenfoot.*;
+import java.util.List;
+import java.util.ArrayList;
+
+/**
+ * Write a description of class MyWorld here.
+ * 
+ * @Noah Keck
+ * @v1.0
+ * @1/6/2018
+ */
+public class MinesweeperBoard extends World
+{
+    public static GreenfootImage GraySquare;
+    public static GreenfootImage ClickedSquare;
+    public static GreenfootImage RedMine;
+    public static GreenfootImage Flagged;
+    public static GreenfootImage Mine;
+    public static GreenfootImage DefusedMine;
+    public static GreenfootImage OneSquare;
+    public static GreenfootImage TwoSquare;
+    public static GreenfootImage ThreeSquare;
+    public static GreenfootImage FourSquare;
+    public static GreenfootImage FiveSquare;
+    public static GreenfootImage SixSquare;
+    public static GreenfootImage SevenSquare;
+    public static GreenfootImage EightSquare;
+    
+    private int width;
+    private int height;
+    private ArrayList<ArrayList<Cell>> cells;
+    public boolean playGame;
+    
+    public MinesweeperBoard()
+    {    
+        super(60, 30, 16);
+        width=60;
+        height=30;
+        LoadContent();
+        CreateCells();
+        GenerateMines();
+        playGame = true;
+    }
+    public void act()
+    {
+        
+    }
+    private void LoadContent()
+    {
+        GraySquare= new GreenfootImage("GreySquareNormal.png");
+        ClickedSquare= new GreenfootImage("DepressedGraySquare.png");
+        RedMine= new GreenfootImage("RedMine.png");
+        Flagged= new GreenfootImage("FlaggedSquare.png");
+        Mine= new GreenfootImage("MineSquare.png");
+        DefusedMine= new GreenfootImage("NotMine.png");
+        OneSquare=new GreenfootImage("1Square.png");
+        TwoSquare=new GreenfootImage("2Square.png");
+        ThreeSquare=new GreenfootImage("3Square.png");
+        FourSquare=new GreenfootImage("4Square.png");
+        FiveSquare=new GreenfootImage("5Square.png");
+        SixSquare=new GreenfootImage("6Square.png");
+        SevenSquare=new GreenfootImage("7Square.png");
+        EightSquare=new GreenfootImage("8Square.png");
+    }
+    private void GenerateMines()
+    {
+        for (int x=0; x<=((width*height)/8); x++)
+            cells.get(getRandomNumber(width+1)).get(getRandomNumber(height+1)).setCell("mine");
+    }
+    private void CreateCells()
+    {
+        cells = new ArrayList<ArrayList<Cell>>();
+        while (cells.size() <= width)
+            cells.add(new ArrayList<Cell>());
+        for (int x=0; x<=60; x++){
+            for (int y=0; y<=30; y++){
+                cells.get(x).add(new Cell());
+                addObject(cells.get(x).get(y), x, y);
+            }
+        }
+    }
+    public void revealZeros(int x, int y) {
+        if (x < 0 || x > width || y < 0 || y > height)
+            return; // check for bounds
+        Cell temp = cells.get(x).get(y);
+        if (temp.findPublicNumber() == 0 && temp.cellType.equals("normal")) {
+            temp.setCell("clicked");
+            temp.setImage(ClickedSquare);
+            revealZeros( x+1, y );
+            revealZeros( x+1, y+1 );
+            revealZeros( x-1, y );
+            revealZeros( x-1, y-1 );
+            revealZeros( x, y-1 );
+            revealZeros( x+1, y-1 );
+            revealZeros( x, y+1 );
+            revealZeros( x-1, y+1 );
+        }
+        else if (temp.cellType.equals("normal")){
+            temp.assignNumberImage(temp.findPublicNumber());
+            return;
+        }
+        else
+            return;
+    }
+    private void revealMines()
+    {
+        for (ArrayList<Cell> cellArray : cells){
+            for (Cell temp : cellArray){
+                if (temp.cellType.equals("mine")){
+                    temp.setImage(Mine);
+                    delay(1);
+                }
+                else if (temp.cellType.equals("flaggedMine")){
+                    temp.setImage(DefusedMine);
+                    delay(1);
+                }
+            }
+        }
+    }
+    public void GameOver()
+    {
+        playGame = false;
+        revealMines();
+        showText("Game Over", (width/2), (height/2));
+        stop();
+    }
+}
